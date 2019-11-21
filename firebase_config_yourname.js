e3e3a3f9ba8e8920de386d305ce699a816858216
Carly Orr <carly_wong_orr@bcit.ca>
2019-11-20 22:48:35 -0800

    added config firebase template

---
+++ firebase_config_yourname.js
@@ -0,0 +1,10 @@
+
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+            //your stuff
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    // Create a reference to the firestore database
+    var db = firebase.firestore();
+
